012430262233530ac1b41e15bf322fffe3a0a1d1
sewon <tpdnjs2896@naver.com>
2025-07-31 14:27:55 +0900

    패키지 추가

---
+++ src/main/java/com/spring/ion/psw/controller/Info_commentController.java
@@ -0,0 +1,4 @@
+package com.spring.ion.psw.controller;
+
+public class Info_commentController {
+}
---
+++ src/main/java/com/spring/ion/psw/repository/Info_commentRepository.java
@@ -0,0 +1,7 @@
+package com.spring.ion.psw.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Info_commentRepository {
+}
