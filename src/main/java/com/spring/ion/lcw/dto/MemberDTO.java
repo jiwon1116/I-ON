b51f17c04233b5fb8ea8211038a61c5381c223f2
LCW <lcwsky28@gmail.com>
2025-08-01 17:07:54 +0900

    로그인/로그아웃 구현

---
+++ src/main/java/com/spring/ion/lcw/dto/MemberDTO.java
@@ -0,0 +1,22 @@
+package com.spring.ion.lcw.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Data
+@ToString
+public class MemberDTO {
+    private Long id;
+    private String userId;
+    private String password;
+    private String nickname;
+    private String gender;
+    private String region;
+    private String profile_img;
+    private Boolean enrollment_verified;
+    private Integer trust_score;
+    private Timestamp created_at;
+    private Boolean enabled;
+}
